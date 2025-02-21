f70347d988f201d2f3cdd8f731492b7355b1b283
Vivekraj0916 <vivekraj0916@gmail.com>
2025-02-21 17:55:12 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.get('/bfhl', (req, res) => {
+    res.status(200).json({
+        operation_code: 1
+    });
+});
+
+app.post('/bfhl', (req, res) => {
+    const { data } = req.body;
+
+
+    const userInfo = {
+        user_id: "vivekraj07",
+        email: "22BCS16334@cuchd.in",
+        roll_number: "22BCS16334"
+    };
+
+    if (!Array.isArray(data)) {
+        return res.status(400).json({
+            is_success: false,
+            ...userInfo,
+            numbers: [],
+            alphabets: [],
+            highest_alphabet: []
+        });
+    }
+
+   
+    const numbers = data.filter(item => !isNaN(item));
+    const alphabets = data.filter(item => /^[a-zA-Z]$/.test(item));
+    const highest_alphabet = alphabets.length > 0 ? [alphabets.sort().pop().toUpperCase()] : [];
+
+
+    res.json({
+        is_success: true,
+        ...userInfo,
+        numbers,
+        alphabets,
+        highest_alphabet
+    });
+});
+
+
+const PORT = process.env.PORT || 5005;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
